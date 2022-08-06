e5bd8c2a1ce5ff09c73f0edafa6e9c3530108b18
BlackbirdXX <superblackbirdzz@gmail.com>
2022-08-06 13:21:57 +0300

    Рабочий код программы

--- TotalWork/Program.cs
+++ TotalWork/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using static System.Console;
 Clear();
-
+WriteLine("Программа, которая из имеющегося массива строк формирует массив из строк,\nдлинна которых меньше или равна 3 символа.\nПервоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.");
+WriteLine("Введите элементы массива по очереди");
 string[] newString = new string[5];
 for (int i = 0; i < 5; i++)
 {
     newString[i] = ReadLine();
 }
+WriteLine("\nВведенный массив");
 PrintString(newString);
+WriteLine("Новый массив, с отсеяными элементами размером больше 3.");
 PrintString(DropoutString(newString));
 
 string[] DropoutString(string[] str)
@@ -34,7 +37,7 @@ void PrintString(string[] str)
 {
     for (int i = 0; i < str.Length - 1; i++)
     {
-        Write($"{str[i]} ");
+        Write($"{str[i]}, ");
     }
     WriteLine($"{str[str.Length - 1]}\n");
 }
