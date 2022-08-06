ccba9f9e085ac4d680119bee8d5376d7b727ec09
BlackbirdXX <superblackbirdzz@gmail.com>
2022-08-06 11:48:25 +0300

    Алгоритм ввода массива строки

--- TotalWork/Program.cs
+++ TotalWork/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using static System.Console;
+Clear();
+
+string[] newString = new string[5];
+for (int i = 0; i < 5; i++)
+{
+    newString[i] = ReadLine();
+}
+PrintString(newString);
+void PrintString(string[] str)
+{
+    for (int i = 0; i < str.Length - 1; i++)
+    {
+        Write($"{str[i]}, ");
+    }
+    WriteLine($"{str[str.Length - 1]}\n");
+}
