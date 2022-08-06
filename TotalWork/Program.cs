b2ddbf3e6f562cdf569784ce5bbf15a4a5fa8fa4
BlackbirdXX <superblackbirdzz@gmail.com>
2022-08-06 13:10:50 +0300

    добавлен алгоритм создания нового массива

--- TotalWork/Program.cs
+++ TotalWork/Program.cs
@@ -8,11 +8,33 @@ for (int i = 0; i < 5; i++)
     newString[i] = ReadLine();
 }
 PrintString(newString);
+PrintString(DropoutString(newString));
+
+string[] DropoutString(string[] str)
+{
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3) count++;
+    }
+    string[] resultString = new string[count];
+    int ctr = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3)
+        {
+            resultString[ctr] = str[i];
+            ctr++;
+        }
+    }
+    return resultString;
+}
+
 void PrintString(string[] str)
 {
     for (int i = 0; i < str.Length - 1; i++)
     {
-        Write($"{str[i]}, ");
+        Write($"{str[i]} ");
     }
     WriteLine($"{str[str.Length - 1]}\n");
 }
